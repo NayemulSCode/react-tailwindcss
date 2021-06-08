e0eb50e2c6beda35fb89de763d65906ef91f94a9
Nayemul <nayemulhasan2016@gmail.com>
2021-06-09 05:02:57 +0600

    add dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router';
+import Dropdown from './components/Dropdown';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Main from './pages';
@@ -6,9 +8,28 @@ import About from './pages/about';
 import Menu from './pages/product';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle =()=>{
+    setIsOpen(!isOpen);
+  }
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown toggle={toggle} isOpen={isOpen} />
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/about" component={About} />
